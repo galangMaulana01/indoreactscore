3f6d75e2bb41cc68f3b0fff808103ab90e9905e5
galangMaulana01 <maulanagebril99@gmail.com>
2026-06-11 02:11:18 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  content: ["./app/**/*.{js,jsx}"],
+  content: [
+    "./app/**/*.{js,jsx}"
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        merah: "#FC0B12",
+        kuning: "#F7CC0C",
+        latar: "#121212",
+        kartu: "#191919",
+      }
+    },
   },
   plugins: [],
 };
